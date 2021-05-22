9b60d3399e2b0522a8cd7ee11c6af842d3cf1ea3
AhmedOwied <ahmedowied91@gamil.com>
2021-05-22 22:39:51 +0200

    Add project files.

---
+++ Migrations/202105211912427_AddColumnToUser.cs
@@ -0,0 +1,18 @@
+﻿namespace JobOfferWebiste.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddColumnToUser : DbMigration
+    {
+        public override void Up() //Add
+        {
+            AddColumn("dbo.AspNetUsers", "UserType", c => c.String());
+        }
+        
+        public override void Down() //Delete
+        {
+            DropColumn("dbo.AspNetUsers", "UserType");
+        }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace JobOfferWebiste.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name ="نوع الوظيفة")]
+        public string CategoryName { get; set; }
+
+        [Required]
+        [Display(Name ="وصف الوظيفة")]
+        public string CategoryDescription { get; set; }
+    }
+}
